0f605480f0324e222936c851f6d510597a7758ad
jayanth <vjayanth1291@gmail.com>
2018-06-28 23:06:16 +0530

    successfully completed

--- doublelinkedlist.py
+++ doublelinkedlist.py
@@ -10,8 +10,51 @@ def main():
             self.head = None
 
         def insertFirst(self,ele):
-                           
-
+            print(self.head)
+            if self.head is None:
+                self.head = node(data=ele)
+            else:
+                curr = node(data=ele)
+                print(self.head.prev)
+                self.head.prev = curr
+                curr.next = self.head
+                self.head = curr
+        def insertLast(self,ele):
+            if self.head is None:
+                self.head = node(data=ele)
+            else:
+                curr = self.head
+                while curr.next:
+                    curr = curr.next
+                temp = node(data=ele)
+                curr.next = temp
+                temp.prev = curr               
+        def print(self):
+            curr = self.head
+            while curr:
+                print(curr.data + '->',end = '')
+                curr = curr.next
+        def search(self,ele):
+            i = 0
+            curr = self.head
+            while curr:
+                if curr.data == ele:
+                    print('element found at' , i)
+                    return
+                else:
+                    i = i+1
+                    curr = curr.next
+            print('element cannot found')
+        def delete(self,ele):
+            curr = self.head
+            while curr:  
+                if curr.data == ele:
+                    temp = curr.prev
+                    temp.next = curr.next
+                    print("element deleted " , ele)
+                    return
+                else:
+                    curr = curr.next
     a = doublelinkedlist()    
     while True:
         print('press 1 to insert last')
@@ -30,10 +73,10 @@ def main():
             ele = input('enter element to enter ')
             a.insertFirst(ele)    
         elif res == '3':
-            ele = input('search element')
+            ele = input('search element ')
             a.search(ele)
         elif res == '4':
-            ele = input('delete element')
+            ele = input('delete element ')
             a.delete(ele)         
         elif res == '5':
             a.print()
